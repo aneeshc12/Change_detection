c95fae38c76c7754b2e93de8bfde0e86079fc198
Vineeth Bhat <vineeth.bhat@gmail.com>
2024-03-04 00:37:02 +0530

    Fix new issues that came up after merge

--- large_dataset_loc.py
+++ large_dataset_loc.py
@@ -11,15 +11,26 @@ class LocalArgs:
     device: str='cuda'
     sam_checkpoint_path: str = '/scratch/vineeth.bhat/sam_vit_h_4b8939.pth'
     ram_pretrained_path: str = '/scratch/vineeth.bhat/ram_swin_large_14m.pth'
-    sampling_period: int = 40
+    sampling_period: int = 80
     downsampling_rate: int = 5 # downsample points every these many frames
     save_dir: str = "/scratch/vineeth.bhat/vin-experiments/large_dataset_trials/8-rooms-new"
-    start_file_index: int = 1
+    start_file_index: int = 2
     last_file_index: int = -1
     rot_correction: float = 0.0 # keep as 30 for 8-room-new 
-    look_around_range: int = 1 # number of sucessive frames to consider at every frame
+    look_around_range: int = 0 # number of sucessive frames to consider at every frame
     save_individual_objects: bool = False
-    down_sample_voxel_size: float = 0.01
+
+    down_sample_voxel_size: float = 0.01 # best results
+    create_ext_mesh: bool = False
+    save_point_clouds: bool = False
+    fpfh_global_dist_factor: float = 1.5
+    fpfh_local_dist_factor: float = 0.4
+    fpfh_voxel_size: float = 0.05
+    localise_times: int = 1
+
+    loc_results_start_file_index: int = 1
+    loc_results_last_file_index: int = -1
+    loc_results_sampling_period: int = 400
 
 if __name__=="__main__":
     start_time = time.time()
@@ -44,6 +55,8 @@ if __name__=="__main__":
 
     if largs.last_file_index == -1:
         largs.last_file_index = num_files
+    if largs.loc_results_last_file_index == -1:
+        largs.loc_results_last_file_index = num_files
 
     frame_counter = 0
 
@@ -144,4 +157,72 @@ if __name__=="__main__":
     print("\n\n\t---------------------")
     mem.view_memory()
 
+    start_time = time.time()
 
+    tgt = []
+    pred = []
+    trans_errors = []
+    rot_errors = []
+    chosen_assignments = []
+
+    for i in range(largs.loc_results_start_file_index, 
+                   largs.loc_results_last_file_index + 1, 
+                   largs.loc_results_sampling_period):
+        print(f"\n\tLocalizing image {i} currently")
+        image_file_path = os.path.join(largs.test_folder_path, 
+                                    f"rgb/{i}.png")
+        depth_file_path = os.path.join(largs.test_folder_path, 
+                                    f"depth/{i}.npy")
+        pose_file_path = os.path.join(largs.test_folder_path, 
+                                    f"pose/{i}.txt")
+
+        
+        with open(pose_file_path, 'r') as file:
+            pose_dict = file.read()
+        pose_dict = ast.literal_eval(pose_dict)
+        pose_dict = {
+            "position": {
+                "x": pose_dict[0]['x'],
+                "y": pose_dict[0]['y'],
+                "z": pose_dict[0]['z']
+            },
+            "rotation": {
+                "x": pose_dict[1]['x'] + largs.rot_correction,
+                "y": pose_dict[1]['y'],
+                "z": pose_dict[1]['z']
+            }
+        }
+
+        q = Rotation.from_euler('xyz', [r for _, r in pose_dict["rotation"].items()], degrees=True).as_quat()
+        t = np.array([x for _, x in pose_dict["position"].items()])
+        target_pose = np.concatenate([t, q])
+        tgt.append(target_pose)
+
+        estimated_pose, chosen_assignment = mem.localise(image_path=image_file_path, 
+                                        depth_image_path=depth_file_path,
+                                        save_point_clouds=largs.save_point_clouds,
+                                        fpfh_global_dist_factor = largs.fpfh_global_dist_factor, 
+                                        fpfh_local_dist_factor = largs.fpfh_global_dist_factor, 
+                                        fpfh_voxel_size = largs.fpfh_voxel_size)
+
+        print("Target pose: ", target_pose)
+        print("Estimated pose: ", estimated_pose)
+
+        translation_error = np.linalg.norm(target_pose[:3] - estimated_pose[:3]) 
+        rotation_error = QuaternionOps.quaternion_error(target_pose[3:], estimated_pose[3:])
+
+        print("Translation error: ", translation_error)
+        print("Rotation_error: ", rotation_error)
+
+        pred.append(estimated_pose.tolist())
+        trans_errors.append(translation_error)
+        rot_errors.append(rotation_error)
+        chosen_assignments.append(chosen_assignment)
+
+    for i in range(len(trans_errors)):
+        print(f"Pose {i + 1}")
+        print("Translation error", trans_errors[i])
+        print("Rotation errors", rot_errors[i])
+
+    end_time = time.time()
+    print(f"Localization completed in {end_time - start_time} seconds")
